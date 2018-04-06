403685b11ec4d154d3141d9e9c4bcf523b3a0587
LDO <ldo@tut.by>
2018-04-06 14:22:01 +0300

    one more formatting

--- ExcelTabs.py
+++ ExcelTabs.py
@@ -3,24 +3,35 @@ import glob
 import sys
 import xlrd, xlwt
 # import time
-
 allowedChars = set('0123456789')
+
+# alignment
 al = xlwt.Alignment()
 al.horz = xlwt.Alignment.HORZ_CENTER
 al.vert = xlwt.Alignment.VERT_CENTER
 al.wrap = True
 
+# fonts
 fnt = xlwt.Font()
 fnt.bold = True
 fnt.height = 8*20
+
+fnt_bold = xlwt.Font()
+fnt_bold.bold = True
+
+# borders
 borders = xlwt.Borders()
 borders.top = 6 #xlwt.Borders.double
 borders.bottom = 1#xlwt.Borders.thin
 
+# styles
 borders_style = xlwt.XFStyle()
 borders_style.borders = borders
 borders_style.font = fnt
 borders_style.alignment = al
+
+fnt_style = xlwt.XFStyle()
+fnt_style.font = fnt_bold
 
 def process_file(inputfile):
     xl = xlrd.open_workbook(inputfile, on_demand = True, encoding_override="cp1251")
@@ -37,11 +48,13 @@ def process_file(inputfile):
             for col_index in range(sheet.ncols):
                 if row_index == 0:
                     continue
-                if row_index == 5:
+                if row_index < 5 and col_index == 4:
+                    newSheet.write(row_index, col_index, sheet.cell(row_index,col_index).value, fnt_style)
+                elif row_index == 5:
                     newSheet.write(row_index, col_index, sheet.cell(row_index,col_index).value, borders_style)
                 else:
                     newSheet.write(row_index, col_index, sheet.cell(row_index,col_index).value)
-        # newSheet.row(5).set_style(borders_style)
+        newSheet.row(5).set_style(borders_style)
         newSheet.row(5).height_mismatch = 1
         newSheet.row(5).height = 650
         newSheet.col(0).width = 256*2
